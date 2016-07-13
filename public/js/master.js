4acdd3d7684f3485d1e32c988ffff6cfa49852d6
TenTail <tentail10@gmail.com>
2016-07-13 14:55:11 +0800

    footer置底

--- public/js/master.js
+++ public/js/master.js
@@ -1,9 +1,14 @@
 $(document).ready(function() {
 
+    var s = $(document).height();
+    var header_h = $('.header').height();
+    var nav_h = $('.navigation').height();
+    var footer_h = $('.footer').height();
+
+    $('#belowtopnav').css('min-height', s-header_h-nav_h-footer_h);
+
     $(window).scroll(function () { 
-
         // console.log($(window).scrollTop());
-
         if ($(window).scrollTop() > 300) {
             $(".navigation").addClass("navbar-fixed-top");
             $("#belowtopnav").css({"padding-top" : "50px"});
