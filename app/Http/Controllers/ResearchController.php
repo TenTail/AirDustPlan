3bbc2361b4098c1f744251e8bc54f7ca460c7996
TenTail <tentail10@gmail.com>
2016-08-19 02:22:53 +0800

    修正圖表類型與優化SQL ver2

--- app/Http/Controllers/ResearchController.php
+++ app/Http/Controllers/ResearchController.php
@@ -14,33 +14,57 @@ class ResearchController extends Controller
 {
     /**
      * 研究的測站
+     *
+     * @var array
      */
     private $r_sitename = ['斗六', '宜蘭', '萬里', '淡水'];
 
     /**
-     * 測站資料
+     * 測站月加總資料
+     *
+     * @var array
      */
     private $r_sum_data = array(
-            '斗六' => array(),
-            '宜蘭' => array(),
-            '萬里' => array(),
-            '淡水' => array(),
+            '斗六' => array(
+                '2010' => array(),'2011' => array(),'2012' => array(),'2013' => array(),'2014' => array(),'2015' => array(),
+            ),
+            '宜蘭' => array(
+                '2010' => array(),'2011' => array(),'2012' => array(),'2013' => array(),'2014' => array(),'2015' => array(),
+            ),
+            '萬里' => array(
+                '2010' => array(),'2011' => array(),'2012' => array(),'2013' => array(),'2014' => array(),'2015' => array(),
+            ),
+            '淡水' => array(
+                '2010' => array(),'2011' => array(),'2012' => array(),'2013' => array(),'2014' => array(),'2015' => array(),
+            )
         );
 
     /**
-     * 測站資料
+     * 測站月平均資料
+     *
+     * @var array
      */
     private $r_avg_data = array(
-            '斗六' => array(),
-            '宜蘭' => array(),
-            '萬里' => array(),
-            '淡水' => array(),
+            '斗六' => array(
+                '2010' => array(),'2011' => array(),'2012' => array(),'2013' => array(),'2014' => array(),'2015' => array(),
+            ),
+            '宜蘭' => array(
+                '2010' => array(),'2011' => array(),'2012' => array(),'2013' => array(),'2014' => array(),'2015' => array(),
+            ),
+            '萬里' => array(
+                '2010' => array(),'2011' => array(),'2012' => array(),'2013' => array(),'2014' => array(),'2015' => array(),
+            ),
+            '淡水' => array(
+                '2010' => array(),'2011' => array(),'2012' => array(),'2013' => array(),'2014' => array(),'2015' => array(),
+            )
         );
 
     /**
      * 研究年份
+     *
+     * @var int
      */
-    private $r_year = ['2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015'];
+    private $r_year;
     
     /**
      * home
@@ -56,24 +80,24 @@ class ResearchController extends Controller
     public function average()
     {
         $result = DB::select('
-            SELECT `sitename`, SUM(`pm25`) AS SUM_PM25, AVG(`pm25`) AS AVG_PM25, SUBSTR(`publish_time`, 1, 7) AS p_time
-            FROM (SELECT * FROM `airpollutions` WHERE `sitename` IN ("斗六", "宜蘭", "萬里", "淡水")) AS Table1
-            WHERE (`publish_time` LIKE "2014%") AND `pm25` > 0
-            GROUP BY  `sitename`, SUBSTR(`publish_time`, 1, 7)
+            SELECT t1.sitename, SUM(`pm25`) AS SUM_PM25, AVG(`pm25`) AS AVG_PM25,SUBSTR(t1.publish_time, 1, 7) AS p_time
+            FROM `airpollutions` as t1
+            inner join ( SELECT `id` FROM `airpollutions` WHERE `sitename` IN ("斗六", "宜蘭", "萬里", "淡水") AND `pm25` > 0 ) as t2
+            on t1.id=t2.id AND SUBSTR(t1.publish_time, 1, 4) IN ("2010", "2011", "2012", "2013", "2014", "2015")
+            GROUP BY t1.sitename, SUBSTR(t1.publish_time, 1, 7)
+            ORDER BY SUBSTR(t1.publish_time, 1, 7) ASC 
             ');
-
-        $month = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"];
+        
         foreach ($result as $key => $value) {
             $this->distribution($value->sitename, $value->SUM_PM25, $value->AVG_PM25, $value->p_time);
         }
-
+        // dd($this->r_sum_data);
         $data = [
             'r_sum_data' => $this->r_sum_data,
             'r_avg_data' => $this->r_avg_data
         ];
-        // dd(json_encode($this->r_sum_data['斗六']));
+        
         return view('research.average', $data);
-        // return view('research.average')->with('r_sum_data', $this->r_sum_data);
     }
 
     /**
@@ -81,7 +105,15 @@ class ResearchController extends Controller
      */
     public function distribution($sitename, $sum, $avg, $p_time)
     {
-        array_push($this->r_sum_data[$sitename], [$p_time, $sum]);
-        array_push($this->r_avg_data[$sitename], [$p_time, $avg]);
+        $month = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"];
+        if (count($this->r_sum_data[$sitename][substr($p_time, 0, 4)]) < 12) {
+            while ($month[count($this->r_sum_data[$sitename][substr($p_time, 0, 4)])] != substr($p_time, 5, 2)) {
+                $f_time = substr($p_time, 0, 4)."-".$month[count($this->r_sum_data[$sitename][substr($p_time, 0, 4)])];
+                array_push($this->r_sum_data[$sitename][substr($p_time, 0, 4)], [$f_time, null]);
+                array_push($this->r_avg_data[$sitename][substr($p_time, 0, 4)], [$f_time, null]);
+            }
+        }
+        array_push($this->r_sum_data[$sitename][substr($p_time, 0, 4)], [$p_time, $sum]);
+        array_push($this->r_avg_data[$sitename][substr($p_time, 0, 4)], [$p_time, $avg]);
     }
 }
--- resources/views/research/average.blade.php
+++ resources/views/research/average.blade.php
@@ -20,99 +20,89 @@
 </style>
 
 <div class="col-md-12">
-    <div class="col-md-6">
-        <h2 class="sitename">斗六</h2> 
-        <div id="斗六" class="chart"></div>
-    </div>
-    <div class="col-md-6">
-        <h2 class="sitename">宜蘭</h2> 
-        <div id="宜蘭" class="chart"></div>
-    </div>
-    <div class="col-md-6">
-        <h2 class="sitename">萬里</h2> 
-        <div id="萬里" class="chart"></div>
-    </div>
-    <div class="col-md-6">
-        <h2 class="sitename">淡水</h2> 
-        <div id="淡水" class="chart"></div>
-    </div>
-
+    <h2 class="sitename">斗六</h2>
+    <div class="chart" id="斗六"></div>
+    <h2 class="sitename">宜蘭</h2>
+    <div class="chart" id="宜蘭"></div>
+    <h2 class="sitename">萬里</h2>
+    <div class="chart" id="萬里"></div>
+    <h2 class="sitename">淡水</h2>
+    <div class="chart" id="淡水"></div>
 </div>
+
 @endsection
 
 @section('page-javascript')
 <script>
-    var chart = {
-        type: 'column',
-    };
     var title = {
         text: ''
     }
     var xAxis = {
         type: 'category',
         labels: {
-            rotation: -45,
             style: {
                 fontSize: '13px',
                 // fontFamily: 'Verdana, sans-serif'
             }
-        }
+        },
+        categories: ['一月', '二月', '三月', '四月', '五月', '六月',
+                '七月', '八月', '九月', '十月', '十一月', '十二月']
     };
     var yAxis = {
-        max: 60,
+        max: 80,
         min: 0,
         title: {
             text: 'PM2.5濃度平均值'
         }
     };
     var legend = {
-        enabled: false
+        enabled: true
     };
 
     $(function () {
         $('#斗六').highcharts({
-            chart: chart,
             title: title,
             xAxis: xAxis,
             yAxis: yAxis,
             legend: legend,
-            series: [{
-                name: '斗六',
-                data: {!! json_encode($r_avg_data['斗六']) !!}
-            }]
+            series: [
+            @for ($i = 2010; $i <= 2015; $i++)
+                {!! "{name: '$i',data: ".json_encode($r_avg_data['斗六'][$i])."}," !!}
+            @endfor
+            ]
         });
         $('#宜蘭').highcharts({
-            chart: chart,
             title: title,
             xAxis: xAxis,
             yAxis: yAxis,
             legend: legend,
-            series: [{
-                name: '宜蘭',
-                data: {!! json_encode($r_avg_data['宜蘭']) !!}
-            }]
+            series: [
+            @for ($i = 2010; $i <= 2015; $i++)
+                {!! "{name: '$i',data: ".json_encode($r_avg_data['宜蘭'][$i])."}," !!}
+            @endfor
+            ]
         });
         $('#萬里').highcharts({
-            chart: chart,
             title: title,
             xAxis: xAxis,
             yAxis: yAxis,
             legend: legend,
-            series: [{
-                name: '萬里',
-                data: {!! json_encode($r_avg_data['萬里']) !!}
-            }]
+            series: [
+            @for ($i = 2010; $i <= 2015; $i++)
+                {!! "{name: '$i',data: ".json_encode($r_avg_data['萬里'][$i])."}," !!}
+            @endfor
+            ]
         });
         $('#淡水').highcharts({
-            chart: chart,
             title: title,
             xAxis: xAxis,
             yAxis: yAxis,
             legend: legend,
-            series: [{
-                name: '淡水',
-                data: {!! json_encode($r_avg_data['淡水']) !!}
-            }]
+            series: [
+            @for ($i = 2010; $i <= 2015; $i++)
+                {!! "{name: '$i',data: ".json_encode($r_avg_data['淡水'][$i])."}," !!}
+            @endfor
+            ]
         });
     });
 </script>
