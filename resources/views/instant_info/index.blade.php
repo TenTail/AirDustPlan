b129418613bff5f74691b108432cb6b032a6fa46
PHChenGit <chenpohsun1@gmail.com>
2016-09-18 02:48:51 +0800

    建立監測站地理圖資 json 檔案，並將即時資訊加入 Google Map 完成多個監測站位置標示

--- resources/views/instant_info/index.blade.php
+++ resources/views/instant_info/index.blade.php
@@ -12,7 +12,7 @@
 @section("title", "空塵計")
 
 @section("content")
-	<table border="1" id="county_table" align="center">
+	{{-- <table id="county_table" align="center">
 		@for($i = 0 ; $i < count($data) ; $i++)
 			@if($i < 5)
 				<td><button type="button" class="btn btn-primary" style="width: 100%;font-size: 20pt;" value="{!! $data[$i] !!}" onclick="getInstantValue(this.value)">{!! $data[$i] !!}</button></td>
@@ -31,127 +31,209 @@
 			@endif
 			
 		@endfor
-	</table>
-	{{-- <table id="county_table" align="center">
-		<tr>
-			<td><button type="button" class="btn btn-success" style="width: 100%;font-size: 20pt; padding:0" value="臺北市
-			" onclick="getInstantValue()">臺北市</button></td>
-			<td><button type="button" class="btn btn-success" style="width: 100%;font-size: 20pt; padding:0" value="新北市" onclick="getInstantValue()">新北市</button></td>
-			<td><button type="button" class="btn btn-success" style="width: 100%;font-size: 20pt; padding:0" value="基隆市" onclick="getInstantValue()">基隆市</button></td>
-			<td><button type="button" class="btn btn-success" style="width: 100%;font-size: 20pt; padding:0" value="桃園市" onclick="getInstantValue()">桃園市</button></td>
-			<td><button type="button" class="btn btn-success" style="width: 100%;font-size: 20pt; padding:0" value="新竹市" onclick="getInstantValue()">新竹市</button></td>
-		</tr>
-		<tr>
-			<td><button style="font-size:16px" value="新竹縣" onclick="getInstantValue()">新竹縣</button></td>
-			<td><button style="font-size:16px" value="苗栗縣" onclick="getInstantValue()">苗栗縣</button></td>
-			<td><button style="font-size:16px" value="臺中市" onclick="getInstantValue()">臺中市</button></td>
-			<td><button style="font-size:16px" value="彰化縣" onclick="getInstantValue()">彰化縣</button></td>
-			<td><button style="font-size:16px" value="南投縣" onclick="getInstantValue()">南投縣</button></td>
-		</tr>
-			<tr>
-			<td><button style="font-size:16px" value="雲林縣" onclick="getInstantValue()">雲林縣</button></td>
-			<td><button style="font-size:16px" value="嘉義市" onclick="getInstantValue()">嘉義市</button></td>
-			<td><button style="font-size:16px" value="嘉義縣" onclick="getInstantValue()">嘉義縣</button></td>
-			<td><button style="font-size:16px" value="臺南市" onclick="getInstantValue()">臺南市</button></td>
-			<td><button style="font-size:16px" value="高雄市" onclick="getInstantValue()">高雄市</button></td>
-		</tr>
-			<tr>
-			<td><button style="font-size:16px" value="屏東縣" onclick="getInstantValue()">屏東縣</button></td>
-			<td><button style="font-size:16px" value="宜蘭縣" onclick="getInstantValue()">宜蘭縣</button></td>
-			<td><button style="font-size:16px" value="花蓮縣" onclick="getInstantValue()">花蓮縣</button></td>
-			<td><button style="font-size:16px" value="臺東縣" onclick="getInstantValue()">臺東縣</button></td>
-			<td><button style="font-size:16px" value="澎湖縣" onclick="getInstantValue()">澎湖縣</button></td>
-		</tr>
 	</table> --}}
-	<div id="show_data">
+{{-- 	<div id="psi_data">
 		
 	</div>
+	<div id="pm25_data">
+		
+	</div> --}}
+	<div id="map" style="width: 100%; height:400px"></div>
 @endsection
 
 @section("page-javascript")
 <script type="text/javascript">
-	function getInstantValue(county)
-	{
-		$.ajaxSetup({
-	        headers: {
-	            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-	        }
+	// $('#psi_data').ready(getInstantValue("雲林縣"));
+
+	// function getInstantValue(county)
+	// {
+	// 	$.ajaxSetup({
+	//         headers: {
+	//             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+	//         }
+	// 	});
+	// 	$.ajax({
+	// 		url: 'instant_info',
+	// 	    type: 'post',
+	// 	    data: {county: county},
+	// 	    // dataType: 'JSON',
+	// 	    success: function (data) {
+	// 	        // alert("success");
+	// 	       	console.log(data);
+
+	// 	       	// Create a chart for PSI
+	//        		var seriesOptions = [], seriesCounter = 0;
+	// 		  	$(function () {
+
+	// 			    function createPsiChart() {
+	// 			    	$('#psi_data').highcharts({
+	// 				        chart: {
+	// 				            type: 'column'
+	// 				        },
+	// 				        title: {
+	// 				            text: data[0].county
+	// 				        },
+	// 				        xAxis: {
+	// 				            categories: [
+	// 				                data[0].publish_time
+	// 				            ],
+	// 				            crosshair: true
+	// 				        },
+	// 				        yAxis: {
+	// 				            min: 0,
+	// 				            title: {
+	// 				                text: 'PSI'
+	// 				            }
+	// 				        },
+	// 				        tooltip: {
+	// 				            // headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+	// 				            pointFormat: '<table><tr><td style="color:{series.color}; padding:0; font-size:16px">{series.name}: </td>' +
+	// 				                '<td style="padding:0; font-size:16px"><b>{point.y:.1f}</b></td></tr>',
+	// 				            footerFormat: '</table>',
+	// 				            shared: true,
+	// 				            useHTML: true
+	// 				        },
+	// 				        plotOptions: {
+	// 				            column: {
+	// 				                pointPadding: 1,
+	// 				                borderWidth: 2
+	// 				            }
+	// 				        },
+	// 				        series: seriesOptions
+	// 				    });
+	// 			    };
+
+	// 			    $.each(data, function (i, name) {
+	// 			            seriesOptions[i] = {
+	// 			                name: data[i].sitename,
+	// 			                data: [parseInt(data[i].psi)]
+	// 			            };
+
+	// 			            // As we're loading the data asynchronously, we don't know what order it will arrive. So
+	// 			            // we keep a counter and create the chart when all the data is loaded.
+	// 			            seriesCounter += 1;
+
+	// 			            if (seriesCounter === data.length) {
+	// 			                createPsiChart();
+	// 			            }  
+	// 			    });
+	// 			});
+
+	// 			// Create a chart for PM2.5
+	// 			var seriesOptions = [], seriesCounter = 0;
+	// 		  	$(function () {
+
+	// 			    function createPsiChart() {
+	// 			    	$('#pm25_data').highcharts({
+	// 				        chart: {
+	// 				            type: 'column'
+	// 				        },
+	// 				        title: {
+	// 				            text: data[0].county
+	// 				        },
+	// 				        xAxis: {
+	// 				            categories: [
+	// 				                data[0].publish_time
+	// 				            ],
+	// 				            crosshair: true
+	// 				        },
+	// 				        yAxis: {
+	// 				            min: 0,
+	// 				            title: {
+	// 				                text: 'PM2.5(ug/m^3)'
+	// 				            }
+	// 				        },
+	// 				        tooltip: {
+	// 				            // headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+	// 				            pointFormat: '<table><tr><td style="color:{series.color}; padding:0; font-size:16px">{series.name}: </td>' +
+	// 				                '<td style="padding:0; font-size:16px"><b>{point.y:.1f}</b></td></tr>',
+	// 				            footerFormat: '</table>',
+	// 				            shared: true,
+	// 				            useHTML: true
+	// 				        },
+	// 				        plotOptions: {
+	// 				            column: {
+	// 				                pointPadding: 1,
+	// 				                borderWidth: 2
+	// 				            }
+	// 				        },
+	// 				        series: seriesOptions
+	// 				    });
+	// 			    };
+
+	// 			    $.each(data, function (i, name) {
+	// 			            seriesOptions[i] = {
+	// 			                name: data[i].sitename,
+	// 			                data: [parseInt(data[i].psi)]
+	// 			            };
+
+	// 			            // As we're loading the data asynchronously, we don't know what order it will arrive. So
+	// 			            // we keep a counter and create the chart when all the data is loaded.
+	// 			            seriesCounter += 1;
+
+	// 			            if (seriesCounter === data.length) {
+	// 			                createPsiChart();
+	// 			            }  
+	// 			    });
+	// 			});
+	// 	    },
+	// 	    error: function (e) {
+	// 	    	alert("Something error!");
+	// 	    }
+	// 	});
+	// }	
+
+var map;
+
+function initMap() {
+	console.log("initMap");
+  	map = new google.maps.Map(document.getElementById('map'), {
+    	center: {lat: 23.941027, lng: 121.076728},
+    	zoom: 7
+  	});
+
+  	 var contentString = '<div id="content">'+
+      '<div id="siteNotice">'+
+      '</div>'+
+      '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+      '<div id="bodyContent">'+
+      '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+      'sandstone rock formation in the southern part of the '+
+      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+      'south west of the nearest large town, Alice Springs; 450&#160;km '+
+      '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+      'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+      'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+      'Aboriginal people of the area. It has many springs, waterholes, '+
+      'rock caves and ancient paintings. Uluru is listed as a World '+
+      'Heritage Site.</p>'+
+      '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+      'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+      '(last visited June 22, 2009).</p>'+
+      '</div>'+
+      '</div>';
+
+  var infowindow = new google.maps.InfoWindow({
+    content: contentString
+  });
+  	var marker, i;
+
+	$.get( "{!! './js/air_quality_station_of_geographic_information.json' !!}", function(data) {
+		console.log("json success");
+		$.each(data, function(i, name){
+			marker = new google.maps.Marker({
+				position:new google.maps.LatLng(data[i].TWD97Lat, data[i].TWD97Lon),
+				map:map,
+				title:data[i].sitename
+			});
 		});
-		$.ajax({
-		    url: 'instant_info',
-		    type: 'post',
-		    data: {county: county},
-		    // dataType: 'JSON',
-		    success: function (data) {
-		        alert("success");
-		       	console.log(data);
-		       	// for(var i = 0; i < data.length ; i++) {
-	       		// 	$('#show_data').append('<p>測站名稱 : ' + data[i].sitename + ' 所屬縣市 : ' + data[i].county + ' PSI : ' + data[i].psi + ' 發佈時間 : ' + data[i].publish_time + '</p>');  	
-	       		// };
-	       		var seriesOptions = [], seriesCounter = 0;
-			  	$(function () {
+	});
 
-				    function createChart() {
-				    	$('#show_data').highcharts({
-					        chart: {
-					            type: 'column'
-					        },
-					        title: {
-					            text: data[0].county
-					        },
-					        xAxis: {
-					            categories: [
-					                data[0].publish_time
-					            ],
-					            crosshair: true
-					        },
-					        yAxis: {
-					            min: 0,
-					            title: {
-					                text: 'PSI'
-					            }
-					        },
-					        tooltip: {
-					            // headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-					            pointFormat: '<table><tr><td style="color:{series.color}; padding:0; font-size:16px">{series.name}: </td>' +
-					                '<td style="padding:0; font-size:16px"><b>{point.y:.1f}</b></td></tr>',
-					            footerFormat: '</table>',
-					            shared: true,
-					            useHTML: true
-					        },
-					        plotOptions: {
-					            column: {
-					                pointPadding: 1,
-					                borderWidth: 2
-					            }
-					        },
-					        series: seriesOptions
-					    });
-				    };
-
-				    $.each(data, function (i, name) {
-				            seriesOptions[i] = {
-				                name: data[i].sitename,
-				                data: [parseInt(data[i].psi)]
-				            };
-
-				            // As we're loading the data asynchronously, we don't know what order it will arrive. So
-				            // we keep a counter and create the chart when all the data is loaded.
-				            seriesCounter += 1;
-
-				            if (seriesCounter === data.length) {
-				                createChart();
-				            }  
-				    });
-				});
-		    },
-		    error: function (e) {
-		    	alert("Something error!");
-		    }
-		});
-	}	
-
+	marker.addListener('click', function() {
+    	infowindow.open(map, marker);
+  	});
+}
 
 </script>
-
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCD5dI4ddETACuDY-rUlZH-2Ept65w150Q&callback=initMap" async defer></script>
 @endsection
