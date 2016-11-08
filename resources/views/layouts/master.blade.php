6e2ecc08e127fab83f1edbb9e87983f63cabbc56
PHChenGit <chenpohsun1@gmail.com>
2016-11-08 20:51:46 +0800

    加入 SEO 關鍵字

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,6 +2,7 @@
 <html>
 <head>
     <meta charset="UTF-8">
+    <meta name="KeyWords" content="空氣汙染, 雲林, 虎尾科技大學, PM2.5, 即時空污, 空污資訊"/>
     @yield("csrf-token")
 
     <title>@yield("title")</title>
@@ -20,17 +21,17 @@
 
 </head>
 <body>
-    
+
     @include("layouts.header")
 
     @include("layouts.navigation")
-    
+
     <div class="container" id="belowtopnav">
         @yield("content")
     </div>
 
     @include("layouts.footer")
-    
+
     <!-- javascript -->
     <script src="{{ asset('js/master.js') }}"></script>
     @yield("page-javascript")
