18fca633946dfb27839739c56df1b5158c7910e4
TenTail <tentail10@gmail.com>
2016-11-17 14:59:41 +0800

    fix table name

--- app/Http/Controllers/InstantInfomationController.php
+++ app/Http/Controllers/InstantInfomationController.php
@@ -20,7 +20,7 @@ class InstantInfomationController extends Controller
     public function index()
     {
         $data = array('臺北市','新北市','基隆市','桃園市','新竹市','新竹縣','苗栗縣','臺中市','彰化縣','南投縣','雲林縣', '嘉義縣','嘉義市','臺南市','高雄市','屏東縣', '宜蘭縣','花蓮縣','臺東縣','澎湖縣');
-        $query = DB::table('airpolluctions')->where('sitename', '斗六')->first();
+        $query = DB::table('airpollutions')->where('sitename', '斗六')->first();
 
         return view("instant_info.index")->with('data', $query);
     }
