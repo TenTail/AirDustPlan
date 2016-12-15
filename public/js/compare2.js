fd985eca37c4ff2beb4e56b3345b2e8054f82c19
TenTail <tentail10@gmail.com>
2016-12-16 01:27:35 +0800

    collection的MapReduce超級加速

---
+++ public/js/compare2.js
@@ -0,0 +1,99 @@
+var all_site = [
+    {county: '基隆市', sitename: ['基隆']},
+    {county: '嘉義市', sitename: ['嘉義']},
+    {county: '高雄市', sitename: ['美濃','大寮','橋頭','仁武','鳳山','林園','楠梓','左營','前金','前鎮','小港','復興']},
+    {county: '新北市', sitename: ['汐止','萬里','新店','土城','板橋','新莊','菜寮','林口','淡水','三重','永和']},
+    {county: '臺北市', sitename: ['士林','中山','萬華','古亭','松山','大同','陽明']},
+    {county: '桃園市', sitename: ['桃園','大園','觀音','平鎮','龍潭','中壢']},
+    {county: '新竹縣', sitename: ['湖口','竹東']},
+    {county: '新竹市', sitename: ['新竹']},
+    {county: '苗栗縣', sitename: ['頭份','苗栗','三義']},
+    {county: '臺中市', sitename: ['豐原','沙鹿','大里','忠明','西屯']},
+    {county: '彰化縣', sitename: ['彰化','線西','二林']},
+    {county: '南投縣', sitename: ['南投','竹山','埔里']},
+    {county: '雲林縣', sitename: ['斗六','崙背','臺西','麥寮']},
+    {county: '嘉義縣', sitename: ['新港','朴子']},
+    {county: '臺南市', sitename: ['新營','善化','安南','臺南']},
+    {county: '屏東縣', sitename: ['屏東','潮州','恆春']},
+    {county: '臺東縣', sitename: ['臺東','關山']},
+    {county: '宜蘭縣', sitename: ['宜蘭','冬山']},
+    {county: '花蓮縣', sitename: ['花蓮']},
+    {county: '澎湖縣', sitename: ['馬公']},
+    {county: '連江縣', sitename: ['馬祖']},
+    {county: '金門縣', sitename: ['金門']}
+]
+
+// search county then return index
+function searchSiteIndex(county) {
+    for(var i = 0, length1 = all_site.length; i < length1; i++){
+        if (all_site[i].county == county) {
+            return i
+        }
+    }
+}
+
+// change sitename when county selected
+function loadSite() {
+    var index = searchSiteIndex($('#county').val())
+    $('#sitename').empty() // 清空
+    // 加入新的<option>
+    for(var i = 0, length1 = all_site[index].sitename.length; i < length1; i++){
+        var option = $('<option></option>').attr('value', all_site[index].sitename[i]).text(all_site[index].sitename[i])
+        $('#sitename').append(option)
+    }
+}
+
+// global var
+var year, two_month, sitename, pollution;
+
+// reset checkbox
+function reset(re) {
+    if (re == "date") {
+        $('.input-year:checkbox').map(function() {  
+            if ($(this).prop('checked')) $(this).prop('checked', false);  
+        }).get()
+        $('.input-month:radio').map(function() {  
+            if ($(this).prop('checked')) $(this).prop('checked', false);  
+        }).get()
+     } else {
+        $('.input-pollution:radio').map(function() {  
+            if ($(this).prop('checked')) $(this).prop('checked', false);  
+        }).get()
+     }
+}
+
+// set search data
+function setData() {
+    // get value that selected checkbox
+    year = $('.input-year:checkbox').map(function() {  
+        if ($(this).prop('checked')) return $(this).val();  
+    }).get()
+    // get value that selected radiobox
+    two_month = $('.input-month:radio').map(function() {  
+        if ($(this).prop('checked')) return $(this).val();  
+    }).get()
+    // get value that selected checkbox
+    pollution = $('.input-pollution:radio').map(function() {  
+        if ($(this).prop('checked')) return $(this).val();  
+    }).get()
+    // get sitename
+    sitename = $('#sitename').val()
+    showData()
+}
+
+// show user set data on table
+function showData() {
+    $('#show-year').html(year.length == 0 ? '<span style="color: red;">尚未設定</span>' : "")
+    year.forEach(function (element, index, array) {
+        $('#show-year').append(element+"年,")
+    })
+    $('#show-month').html(two_month.length == 0 ? '<span style="color: red;">尚未設定</span>' : "")
+    two_month.forEach(function (element, index, array) {
+        $('#show-month').append(element+"月~"+(parseInt(element)+1)+"月")
+    })
+    $('#show-site').html(sitename+"站")
+    $('#show-pollution').html(pollution.length == 0 ? '<span style="color: red;">尚未設定</span>' : "")
+    pollution.forEach(function (element, index, array) {
+        $('#show-pollution').append(element+",")
+    })
+}
--- resources/views/history-compare2.blade.php
+++ resources/views/history-compare2.blade.php
@@ -37,7 +37,7 @@
                             <h2 style="text-align: center;">選擇年份</h2>
                             @for ($i = getdate()['year']; $i != 1984; $i--)
                             <div style="width: 25%;float: left;text-align: center;">
-                                <input type="checkbox" class="input-date" id="{{ $i }}" value="{{ $i }}" onchange="setData()">
+                                <input type="checkbox" class="input-year" id="{{ $i }}" value="{{ $i }}" onchange="setData()">
                                 <label for="{{ $i }}" style="padding-right: 10px;">{{ $i."年" }}</label>
                             </div>
                             @endfor
@@ -46,7 +46,7 @@
                             <h2 style="text-align: center;">選擇月份</h2>
                             @for ($i = 1; $i != 12; $i++)
                             <div style="width: 33.33%;float: left;text-align: center;">
-                                <input type="radio" name="month" id="{{ $i }}" value="{{ $i }}" onchange="setData()">
+                                <input type="radio" name="month" class="input-month" id="{{ $i }}" value="{{ $i }}" onchange="setData()">
                                 <label for="{{ $i }}" style="padding-right: 10px;">{{ $i."月~".($i+1)."月" }}</label>
                             </div>
                             @endfor
@@ -117,7 +117,7 @@
                             <?php $pollution = ['PM2.5', 'PM10', 'SO2', 'CO'] ?>
                             @for ($i = 0; $i < count($pollution); $i++)
                             <div style="width: 25%;float: left;text-align: center;">
-                                <input type="checkbox" class="input-pollution" id="{{ $pollution[$i] }}" value="{{ $pollution[$i] }}" onchange="setData()">
+                                <input type="radio" name="pollution" class="input-pollution" id="{{ $pollution[$i] }}" value="{{ $pollution[$i] }}" onchange="setData()">
                                 <label for="{{ $pollution[$i] }}" style="padding-right: 10px;">{{ $pollution[$i] }}</label>
                             </div>
                             @endfor
@@ -168,118 +168,170 @@
 @endsection
 
 @section('page-javascript')
+<script src="{{ asset('js/compare2.js') }}"></script>
 <script>
-    var all_site = [
-        {county: '基隆市', sitename: ['基隆']},
-        {county: '嘉義市', sitename: ['嘉義']},
-        {county: '高雄市', sitename: ['美濃','大寮','橋頭','仁武','鳳山','林園','楠梓','左營','前金','前鎮','小港','復興']},
-        {county: '新北市', sitename: ['汐止','萬里','新店','土城','板橋','新莊','菜寮','林口','淡水','三重','永和']},
-        {county: '臺北市', sitename: ['士林','中山','萬華','古亭','松山','大同','陽明']},
-        {county: '桃園市', sitename: ['桃園','大園','觀音','平鎮','龍潭','中壢']},
-        {county: '新竹縣', sitename: ['湖口','竹東']},
-        {county: '新竹市', sitename: ['新竹']},
-        {county: '苗栗縣', sitename: ['頭份','苗栗','三義']},
-        {county: '臺中市', sitename: ['豐原','沙鹿','大里','忠明','西屯']},
-        {county: '彰化縣', sitename: ['彰化','線西','二林']},
-        {county: '南投縣', sitename: ['南投','竹山','埔里']},
-        {county: '雲林縣', sitename: ['斗六','崙背','臺西','麥寮']},
-        {county: '嘉義縣', sitename: ['新港','朴子']},
-        {county: '臺南市', sitename: ['新營','善化','安南','臺南']},
-        {county: '屏東縣', sitename: ['屏東','潮州','恆春']},
-        {county: '臺東縣', sitename: ['臺東','關山']},
-        {county: '宜蘭縣', sitename: ['宜蘭','冬山']},
-        {county: '花蓮縣', sitename: ['花蓮']},
-        {county: '澎湖縣', sitename: ['馬公']},
-        {county: '連江縣', sitename: ['馬祖']},
-        {county: '金門縣', sitename: ['金門']}
-    ]
-
-    // search county then return index
-    function searchSiteIndex(county) {
-        for(var i = 0, length1 = all_site.length; i < length1; i++){
-            if (all_site[i].county == county) {
-                return i
-            }
-        }
-    }
-
-    // change sitename when county selected
-    function loadSite() {
-        var index = searchSiteIndex($('#county').val())
-        $('#sitename').empty() // 清空
-        // 加入新的<option>
-        for(var i = 0, length1 = all_site[index].sitename.length; i < length1; i++){
-            var option = $('<option></option>').attr('value', all_site[index].sitename[i]).text(all_site[index].sitename[i])
-            $('#sitename').append(option)
-        }
-    }
-
     $(document).ready(function () {
         loadSite()
         setData()
+        // drawChart()
     })
-
-    // reset checkbox
-    function reset(re) {
-        if (re == "date") {
-            $('.input-date:checkbox').map(function() {  
-                if ($(this).prop('checked')) $(this).prop('checked', false);  
-            }).get()
-            $('input:radio').map(function() {  
-                if ($(this).prop('checked')) $(this).prop('checked', false);  
-            }).get()
-         } else {
-            $('.input-pollution:checkbox').map(function() {  
-                if ($(this).prop('checked')) $(this).prop('checked', false);  
-            }).get()
-         }
-    }
-
-    var year, two_month, sitename, pollution;
-
-    // set search data
-    function setData() {
-        // get value that selected checkbox
-        year = $('.input-date:checkbox').map(function() {  
-            if ($(this).prop('checked')) return $(this).val();  
-        }).get()
-        // get value that selected checkbox
-        pollution = $('.input-pollution:checkbox').map(function() {  
-            if ($(this).prop('checked')) return $(this).val();  
-        }).get()
-        // get value that selected radiobox
-        two_month = $('input:radio').map(function() {  
-            if ($(this).prop('checked')) return $(this).val();  
-        }).get()
-        // get sitename
-        sitename = $('#sitename').val()
-        showData()
-    }
-
-    // show user set data on table
-    function showData() {
-        $('#show-year').html(year.length == 0 ? '<span style="color: red;">尚未設定</span>' : "")
-        year.forEach(function (element, index, array) {
-            $('#show-year').append(element+"年,")
-        })
-        $('#show-month').html(two_month.length == 0 ? '<span style="color: red;">尚未設定</span>' : "")
-        two_month.forEach(function (element, index, array) {
-            $('#show-month').append(element+"月~"+(parseInt(element)+1)+"月")
-        })
-        $('#show-site').html(sitename+"站")
-        $('#show-pollution').html(pollution.length == 0 ? '<span style="color: red;">尚未設定</span>' : "")
-        pollution.forEach(function (element, index, array) {
-            $('#show-pollution').append(element+",")
-        })
-    }
 
     // button start draw 
     function startDraw() {
         if (year.length == 0 || two_month.length == 0 || sitename == '' || pollution.length == 0 ) {
             alert('請設定時間、測站、污染物。');
         } else {
-            console.log(year, two_month, sitename, pollution);
+            $.ajaxSetup({
+                headers: { 'X-CSRF-TOKEN': $('meta[name=csrf-token]').attr('content') }
+            })
+            postData = {
+                _token: $('meta[name=csrf-token]').attr('content'),
+                year: year,
+                month: two_month[0],
+                sitename: sitename,
+                pollution: pollution,
+            }
+            $.ajax({
+                type: 'POST',
+                url: '{{ route('history-compare2.compare') }}',
+                data: postData,
+                success: function (data) {
+                    console.log(data)
+                    drawChart(data)
+                },
+                error: function () {
+                    console.log("error")
+                }
+            })
         }
+    }
+    function setY() {
+        if (pollution == "PM2.5") {
+            return [{
+                labels: {
+                    format: '{value}μg/m3',
+                    style: {
+                        color: Highcharts.getOptions().colors[2]
+                    }
+                },
+                title: {
+                    text: 'PM2.5',
+                    style: {
+                        color: Highcharts.getOptions().colors[2]
+                    }
+                },
+                opposite: true
+            }]
+        } else if (pollution == "PM10") {
+            return [{
+                labels: {
+                    format: '{value}μg/m3',
+                    style: {
+                        color: Highcharts.getOptions().colors[0]
+                    }
+                },
+                title: {
+                    text: 'PM10',
+                    style: {
+                        color: Highcharts.getOptions().colors[0]
+                    }
+                },
+                opposite: true
+            }]
+        } else {
+            return [{
+                labels: {
+                    format: '{value}μg/m3',
+                    style: {
+                        color: Highcharts.getOptions().colors[1]
+                    }
+                },
+                title: {
+                    text: 'SO2',
+                    style: {
+                        color: Highcharts.getOptions().colors[1]
+                    }
+                },
+                opposite: true
+            }]
+        }
+    }
+    function setCharts() {
+        return {
+            xAxis: [{
+                type: 'datetime',
+                dateTimeLabelFormats: {
+                    day: '%m/%d',
+                    week: '%m月%e日',
+                },
+                tickInterval: 24 * 60 * 60 * 1000,
+            }],
+            yAxis: setY(),
+            rangeSelector: {
+                allButtonsEnabled: true,
+                buttons: [{
+                    type: 'week',
+                    count: 1,
+                    text: '週'
+                },{
+                    type: 'month',
+                    count: 1,
+                    text: '一個月'
+                },{
+                    type: 'all',
+                    text: '兩個月'
+                }],
+                selected: 2,
+                buttonTheme: {
+                    width: 60
+                },
+                inputDateFormat: '%m月%d日',
+                inputEditDateFormat: '%m月%d日'
+            },
+        }
+            
+    }
+
+    function drawChart(data) {
+        setting = setCharts()
+        console.log(setting)
+        $('#container').highcharts('StockChart', {
+            title: {
+                text: sitename+'站歷史空汙比較圖'
+            },
+            subtitle: {
+                text: two_month[0]+'月~'+(parseInt(two_month[0])+1)+'月之比較'
+            },
+            xAxis: setting.xAxis,
+            yAxis: setting.yAxis,
+            rangeSelector: setting.rangeSelector,
+            tooltip: {
+                shared: false
+            },
+            legend: {
+                enabled: true,
+                layout: 'vertical',
+                align: 'left',
+                x: 20,
+                verticalAlign: 'top',
+                y: 90,
+                floating: true,
+                backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'
+            },
+            series: [{
+                name: 'Rainfall',
+                type: 'column',
+                // yAxis: 1,
+                data: data,
+                tooltip: {
+                    valueSuffix: ' mm'
+                }
+
+            }],
+            credits: {
+                enabled: false
+            },
+        });
     }
 
 </script>
